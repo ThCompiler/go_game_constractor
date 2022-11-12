f7f060168165053f9b2ac7702e82632b5b1d0da4
TheCompiler <vet_v2002@mail.ru>
2022-11-12 17:01:40 +0300

    Update docs file and start work with server generator

--- scg/handler/codegen/main.go
+++ scg/handler/codegen/main.go
@@ -14,21 +14,24 @@ func MainFile(rootPkg string, rootDir string, scriptInfo expr.ScriptInfo) []*cod
 	return []*codegen.File{mainFile}
 }
 
-func generateMain(_ string, rootDir string, scriptInfo expr.ScriptInfo) *codegen.File {
+func generateMain(rootPkg string, rootDir string, scriptInfo expr.ScriptInfo) *codegen.File {
 	var sections []*codegen.SectionTemplate
 
 	fpath := filepath.Join(rootDir, "cmd", "main.go")
 	imports := []*codegen.ImportSpec{
-		codegen.SCGImport(path.Join("director", "scene")),
+		codegen.SCGImport(path.Join("pkg", "convertor", "words")),
+		codegen.SCGImport(path.Join("pkg", "convertor", "words", "languages")),
+		{Path: path.Join(rootPkg, "config")},
+		{Path: path.Join(rootPkg, "internal", "app")},
 	}
 
 	sections = []*codegen.SectionTemplate{
-		codegen.Header(codegen.ToTitle(scriptInfo.Name)+"-Custom user text errors", "errors", imports, false),
+		codegen.Header(codegen.ToTitle(scriptInfo.Name)+"-Main file", "errors", imports, true),
 	}
 
 	sections = append(sections, &codegen.SectionTemplate{
-		Name:   "text-errors",
-		Source: textErrorStructT,
+		Name:   "main-file",
+		Source: mainFileStructT,
 		Data:   scriptInfo,
 		FuncMap: map[string]interface{}{
 			"ToTitle": codegen.ToTitle,
@@ -38,7 +41,10 @@ func generateMain(_ string, rootDir string, scriptInfo expr.ScriptInfo) *codegen
 	return &codegen.File{Path: fpath, SectionTemplates: sections, IsUpdatable: true}
 }
 
-const textErrorStructT = `func main() {
+const mainFileStructT = ` // This is your application's startup file. 
+// All the basic settings take place already in the "internal/app" file. 
+
+func main() {
 	// Configuration
 	cfg, err := config.New{{ ToTitle .Name }}Config()
 	if err != nil {
