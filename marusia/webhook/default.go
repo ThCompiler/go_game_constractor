1d2ff8cd1abd2f275148478fc02f24ab01747b47
TheCompiler <vet_v2002@mail.ru>
2022-11-16 17:29:10 +0300

    Bug fix

--- marusia/webhook/default.go
+++ marusia/webhook/default.go
@@ -1,66 +1,66 @@
 package webhook
 
 import (
-	sd "github.com/ThCompiler/go_game_constractor/director/scriptdirector"
-	"github.com/ThCompiler/go_game_constractor/marusia"
-	"github.com/ThCompiler/go_game_constractor/marusia/runner"
-	"github.com/ThCompiler/go_game_constractor/pkg/logger"
-	"time"
+    sd "github.com/ThCompiler/go_game_constractor/director/scriptdirector"
+    "github.com/ThCompiler/go_game_constractor/marusia"
+    "github.com/ThCompiler/go_game_constractor/marusia/runner"
+    "github.com/ThCompiler/go_game_constractor/pkg/logger"
+    "time"
 )
 
 const RequestTime = 60 * time.Second
 
 func NewDefaultMarusiaWebhook(l logger.Interface, runnerHub runner.ScriptRunner, sdc sd.SceneDirectorConfig) *marusia.Webhook {
-	wh := marusia.NewWebhook(l)
+    wh := marusia.NewWebhook(l)
 
-	wh.OnEvent(func(r marusia.Request) (resp marusia.Response, err error) {
-		err = nil
+    wh.OnEvent(func(r marusia.Request) (resp marusia.Response, err error) {
+        err = nil
 
-		if r.Request.Command == marusia.OnStart || r.Request.Command == "debug" {
-			runnerHub.AttachDirector(r.Session.SessionID, sd.NewScriptDirector(sdc))
-		}
+        if r.Request.Command == marusia.OnStart || r.Session.New {
+            runnerHub.AttachDirector(r.Session.SessionID, sd.NewScriptDirector(sdc))
+        }
 
-		ans := runnerHub.RunScene(ToHubRequest(r))
+        ans := runnerHub.RunScene(ToHubRequest(r))
 
-		ticker := time.NewTicker(RequestTime)
-		select {
-		case answer, ok := <-ans:
-			if ok {
-				resp.Text = answer.Text.BaseText
-				resp.TTS = answer.Text.TextToSpeech
-				resp.EndSession = answer.IsEndOfScript
-				resp.Buttons = toMarusiaButtons(answer.Buttons)
+        ticker := time.NewTicker(RequestTime)
+        select {
+        case answer, ok := <-ans:
+            if ok {
+                resp.Text = answer.Text.BaseText
+                resp.TTS = answer.Text.TextToSpeech
+                resp.EndSession = answer.IsEndOfScript
+                resp.Buttons = toMarusiaButtons(answer.Buttons)
 
-				if answer.IsEndOfScript {
-					answer.WorkedDirector.Close()
-				}
-			} else {
-				err = ErrorBadDirectorAnswer
-			}
-			break
-		case <-ticker.C:
-			err = ErrorTooLongRunning
-			break
-		}
-		ticker.Stop()
+                if answer.IsEndOfScript {
+                    answer.WorkedDirector.Close()
+                }
+            } else {
+                err = ErrorBadDirectorAnswer
+            }
+            break
+        case <-ticker.C:
+            err = ErrorTooLongRunning
+            break
+        }
+        ticker.Stop()
 
-		if err != nil {
-			l.Error(err)
-		}
-		return
-	})
+        if err != nil {
+            l.Error(err)
+        }
+        return
+    })
 
-	return wh
+    return wh
 }
 
 func toMarusiaButtons(buttons []runner.Button) []marusia.Button {
-	res := make([]marusia.Button, 0)
-	for _, button := range buttons {
-		res = append(res, marusia.Button{
-			Title:   button.Title,
-			URL:     button.URL,
-			Payload: button.Payload,
-		})
-	}
-	return res
+    res := make([]marusia.Button, 0)
+    for _, button := range buttons {
+        res = append(res, marusia.Button{
+            Title:   button.Title,
+            URL:     button.URL,
+            Payload: button.Payload,
+        })
+    }
+    return res
 }
